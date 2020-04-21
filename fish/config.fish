257c8668c612434ccda482cb1b7b7abd1a135892
Crunchy Plasma <crunchyplasma@gmail.com>
2020-04-21 20:08:37 +0200

    initial add

---
+++ fish/config.fish
@@ -0,0 +1,23 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+
+fish_vi_key_bindings
+
+# Aliases
+
+alias vi='nvim'
+alias sl='exa'
+alias ls='exa'
+alias la='exa -a'
+alias cr='cd /'
+alias gs='git status'
+
+
+# Exports
+set -x EDITOR nvim
+set -x VISUAL nvim
+set -x TERMINAL termite
+set -x TERM termite
---
+++ spectrwm/baraction.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# baraction.sh for spectrwm status bar
+
+## DATE
+dte() {
+  dte="$(date +"%H:%M | %a, %B %d")"
+  echo -e "$dte"
+}
+
+## DISK
+hdd() {
+  hdd="$(df -h | awk 'NR==4{print $3, $5}')"
+  echo -e "HDD: $hdd"
+}
+
+## RAM
+mem() {
+  mem=`free | awk '/Mem/ {printf "%d MiB/%d MiB\n", $3 / 1024.0, $2 / 1024.0 }'`
+  echo -e "MEM: $mem"
+}
+
+## CPU
+cpu() {
+  read cpu a b c previdle rest < /proc/stat
+  prevtotal=$((a+b+c+previdle))
+  sleep 0.5
+  read cpu a b c idle rest < /proc/stat
+  total=$((a+b+c+idle))
+  cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+  echo -e "CPU: $cpu%"
+}
+
+## VOLUME
+vol() {
+    vol=`pamixer --get-volume-human`
+    echo -e "VOL: $vol"
+}
+
+SLEEP_SEC=5
+#loops forever outputting a line every SLEEP_SEC secs
+while :; do
+    echo "$(dte) | $(cpu) | $(vol)"
+	sleep $SLEEP_SEC
+done
